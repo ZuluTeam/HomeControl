ca87a69d1736faf0d15699917151da6aa71a7e18
Konstantin Zyryanov <kzyryanov@gmail.com>
2022-02-04 10:56:11 +0100

    ✨ Disable idle timer to prevent screen to "sleep"

--- Home Control/Home Control/Home_ControlApp.swift
+++ Home Control/Home Control/Home_ControlApp.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 @main
 struct Home_ControlApp: App {
+    init() {
+        UIApplication.shared.isIdleTimerDisabled = true
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
